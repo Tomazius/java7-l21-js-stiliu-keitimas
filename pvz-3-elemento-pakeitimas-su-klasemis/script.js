cdb2fa5c4d2eabaaabf56d76d7de01a1adec0242
Tomaszius <liutkus.to@gmail.com>
2022-06-21 13:13:16 +0300

    baigtas trecias pvz

--- pvz-3-elemento-pakeitimas-su-klasemis/script.js
+++ pvz-3-elemento-pakeitimas-su-klasemis/script.js
@@ -1,1 +1,19 @@
-window.onload []
+window.onload = function (){
+    let pastraipa = document.getElementById("pastraipa");
+    let mygtukas = document.getElementById("mygtukas");
+
+    mygtukas.onclick = function(){
+        togglepasleptaklase(pastraipa);
+
+    }
+}
+
+
+function togglepasleptaklase(elementas){
+    if (elementas.classList.contains("pasleptas")){
+        elementas.classList.remove("pasleptas");
+    }
+    else{
+        elementas.classList.add("pasleptas");
+    }
+}
